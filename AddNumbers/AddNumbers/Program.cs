d71cd663f26dacebec6893820b6adec13d69c622
Pothirajan <cpothirajan@gmail.com>
2018-08-17 20:17:12 +0530

    Added logic for adding two numbers

--- AddNumbers/AddNumbers/Program.cs
+++ AddNumbers/AddNumbers/Program.cs
@@ -11,6 +11,9 @@ namespace AddNumbers
         static void Main(string[] args)
         {
             //Add Two Numbers
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("The Total is " + x + y);
         }
     }
 }
